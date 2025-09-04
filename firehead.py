7f123db22661fe692c2cb7ee55e3fc3d06618771
Moibe <moi.estrello@gmail.com>
2025-09-04 11:20:59 -0600

    No translate @ head

--- firehead.py
+++ firehead.py
@@ -11,5 +11,6 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-585LHZXF');</script>
-<!-- End Google Tag Manager -->     
+<!-- End Google Tag Manager --> 
+<script>document.documentElement.setAttribute('translate', 'no');</script>    
 """

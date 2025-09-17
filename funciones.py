d411cb903cd2f8bb3241095e0c893e527dacdd7b
Moibe <moi.estrello@gmail.com>
2025-09-17 16:45:25 -0600

    Perform receives arreglo

--- funciones.py
+++ funciones.py
@@ -18,7 +18,7 @@ def perform(input1, gender, personaje, usuario):
 
     gender = gender or "superhero" #default es superhero.
     #Los tokens se checan dentro de perform para estar seguros de que cuenta con los tokens para ejecutar esa operación en particular.
-    tokens = fireWhale.obtenDato('usuarios', usuario, 'tokens')
+    tokens = fireWhale.obtenDato('usuarios', usuario['uid'], 'tokens')
     
     #1: Reglas sobre autorización si se tiene el crédito suficiente.
     if tokens >= globales.costo_work: #Lo hará solo si tiene el crédito suficiente.

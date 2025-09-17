1645f49d9f2119a7b495f30b9b5e385b577af2c1
Moibe <moi.estrello@gmail.com>
2025-09-17 16:23:35 -0600

    gr.JSON para recibir resultado en arreglo

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def welcome(usuario_firebase):
 #fire provee las partes de javascript que se requieren para correr el chequeo de firebase.
 with gr.Blocks(theme=globales.tema, title="Splashmix App", head=firehead.head, js=fire.js, css="footer {visibility: hidden}") as main:
     
-    usuario_firebase = gr.Textbox(visible=False) #Espacio para almacenar el usuario de firebase 
+    usuario_firebase = gr.JSON(visible=False) #Espacio para almacenar el usuario de firebase 
     acheteemeele = gr.HTML("""
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-585LHZXF"

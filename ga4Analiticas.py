bf5f588c2e670e229a859b93767215cf19de2961
Moibe <moi.estrello@gmail.com>
2025-09-17 17:36:46 -0600

    Verbose for g4

--- ga4Analiticas.py
+++ ga4Analiticas.py
@@ -11,6 +11,8 @@ def send_ga4_signup_event(gaCliente):
     Args:
         
     """
+
+    print("Estoy en send sign up y gaCliente es: ", gaCliente)
     url = f"https://www.google-analytics.com/mp/collect?measurement_id={globales.ga4ID}&api_secret={bridges.ga4Key}"
     
     payload = {

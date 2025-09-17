c8555c6a4b26d78e89bb7cfd991bc609df6a1d45
Moibe <moi.estrello@gmail.com>
2025-09-17 13:12:58 -0600

    Fix test

--- firehead.py
+++ firehead.py
@@ -4,7 +4,6 @@ head = """
     <script src="https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore-compat.js"></script>  
     <script>
     console.log("Hola estoy en HEAD.js de firehead.py @ BLOCKS")  
-    console.log("Más texto desde firehead........")
     </script>
 
 <!-- Google Tag Manager -->
@@ -14,7 +13,8 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-585LHZXF');
+console.log("Más texto desde firehead........")
 </script>
 <!-- End Google Tag Manager --> 
-
+<script>document.documentElement.setAttribute('translate', 'no');</script>    
 """

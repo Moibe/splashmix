9d569a058e4d66af9093b42c13888895ec17d69c
Moibe <moi.estrello@gmail.com>
2025-09-17 13:38:34 -0600

    Double bracket tag manager

--- firehead.py
+++ firehead.py
@@ -8,13 +8,13 @@ head = """
 
 <!-- Google Tag Manager -->
 <script>
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({{'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-585LHZXF');
-console.log("Más texto desde firehead:", window.gaClientID)
+}})(window,document,'script','dataLayer','GTM-585LHZXF');
 </script>
 <!-- End Google Tag Manager --> 
-<script>document.documentElement.setAttribute('translate', 'no');</script>    
+<script>document.documentElement.setAttribute('translate', 'no');</script>   
+console.log("Más texto desde firehead:", window.gaClientID) 
 """

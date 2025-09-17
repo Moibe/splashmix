4e7468c2f68b3ce26414b9ffa824965ac974597d
Moibe <moi.estrello@gmail.com>
2025-09-17 13:26:11 -0600

    Display gaClient

--- firehead.py
+++ firehead.py
@@ -13,7 +13,7 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-585LHZXF');
-console.log("Más texto desde firehead........")
+console.log("Más texto desde firehead:", window.gaClientID)
 </script>
 <!-- End Google Tag Manager --> 
 <script>document.documentElement.setAttribute('translate', 'no');</script>    

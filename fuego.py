7d98c7916b811414b9a65fcc9d16f394eaacd987
Moibe <moi.estrello@gmail.com>
2025-09-19 20:33:31 -0600

    Aun así se regresa gaClient

--- fuego.py
+++ fuego.py
@@ -7,6 +7,7 @@ function funcion() {{
     // Verificar si resultado está vacío
     if (!usuario_firebase || usuario_firebase === "" || usuario_firebase === "null" || usuario_firebase === "undefined") {{
     console.log("Resultado está vacío o es null/undefined, redireccionando...");
+    arreglo = {{gaClient: window.gaClientID, uid: None}}
     return null;
     }} else {{
     console.log("Resultado no está vacío, si hay user de firebase, no se redirecciona a login.");

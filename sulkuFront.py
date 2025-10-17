cd638afc43a5ebd65c7f4381d9b9e1d30f0cc51f
Moibe <moi.estrello@gmail.com>
2025-10-17 16:55:50 -0600

    Info @ non-user

--- sulkuFront.py
+++ sulkuFront.py
@@ -40,11 +40,12 @@ def precarga(arreglo):
     gaClient = arreglo.get('gaClient', '')
 
     #uid = 'uQDteq2ezQP6S1KNh1mf80wMYPg1' #Asumimos que ya lo traemos de auth y que aún no se guarda en firestore.
-        
+            
     if uid == None:
         #Aquí tenemos que hacer el redireccionamiento si no hay uid.
         mensaje = 'Necesitas loguearte al sistema.'
         mensaje2 = ''
+        gr.Info(title="¡Bienvenido!", message=mensajes.lbl_info_welcome, duration=None, visible=True)
         return uid, gr.Accordion(label=mensaje, open=True), gr.Button(value="Login 👋🏻"), gr.Accordion(label=mensaje2, open=False)
     
     else: #Si si hubo uid continuas el camino normal. 
@@ -63,7 +64,7 @@ def precarga(arreglo):
                 else: #Si no se encontró significa que el usuario no existe en Firestore y deberíamos crear uno nuevo.
                     #Crear usuario nuevo en firestore, con 5 tokens y guarda su info de email y displayname.
                     print("Camino 2: Usuario Nuevo:") #Aquí tmb registraremos el evento de ga4.
-                    gr.Info(title="¡Bienvenido!", message=mensajes.lbl_info_welcome, duration=None, visible=True)
+                    
                     datos_perfil = {
                     'diplayName': displayName,
                     'email': email,

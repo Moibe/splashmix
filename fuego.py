7e7afb7ada28d1fada0c2dd633474c702db1e21c
Moibe <moi.estrello@gmail.com>
2025-11-26 11:29:15 -0600

    Fuego obtiene country ip

--- fuego.py
+++ fuego.py
@@ -2,19 +2,21 @@ js = f"""
 function funcion() {{
     console.log("Ésto es un console log normal desde fuego . js @ PRECARGA")        
     usuario_firebase = localStorage.getItem('uid');
+    country_ip = localStorage.getItem('country_ip');
     console.log("Éste es el usuario que obtuvo fuego: ", usuario_firebase)
+    console.log("Éste es el country_ip que obtuvo fuego: ", country_ip)
     
     // Verificar si resultado está vacío
     if (!usuario_firebase || usuario_firebase === "" || usuario_firebase === "null" || usuario_firebase === "undefined") {{
     console.log("Resultado está vacío o es null/undefined, redireccionando...");
-    arreglo = {{gaClient: window.gaClientID, uid: null}}
+    arreglo = {{gaClient: window.gaClientID, uid: null, country_ip: country_ip}}
     return arreglo;
     }} else {{
     console.log("Resultado no está vacío, si hay user de firebase, no se redirecciona a login.");
     }}
     console.log("En resultado que se está enviando es:", window.gaClientID)
     console.log(usuario_firebase)
-    arreglo = {{gaClient: window.gaClientID, uid: usuario_firebase}}
+    arreglo = {{gaClient: window.gaClientID, uid: usuario_firebase, country_ip: country_ip}}
     console.log("El arreglo que se está enviando es:" )
     console.log(arreglo)   
     return arreglo     
